34641368d53c9152610955345085561dfb7d3711
Ezequiel Casella <ecasellao@gmail.com>
2020-07-23 12:37:03 -0300

    Change of binding adapter function imageUrl to be an extension function of ImageView

--- app/src/main/java/com/example/ecommerce/marketlist/BindingUtils.kt
+++ app/src/main/java/com/example/ecommerce/marketlist/BindingUtils.kt
@@ -9,27 +9,28 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.ecommerce.R
 import com.example.ecommerce.domain.Banner
 
-//@BindingAdapter("imageUrl")
-//fun ImageView.setImage(imgUrl: String?) {
-//    imgUrl?.let {
-//        val imgUri = it.toUri().buildUpon().scheme("https").build()
-//        Log.i("i/BindingUtils","URI: $imgUri")
-//        Glide.with(this.context)
-//            .load(imgUri)
-////            .apply(RequestOptions().placeholder("image placeholder"))
-//            .into(this)
-//    }
-//}
-private var setImageCalls = 0
 @BindingAdapter("imageUrl")
-fun setImage(imgView: ImageView,imgUrl: String?) {
+fun ImageView.setImage(imgUrl: String?) {
     imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-
-        setImageCalls++
-        Log.i("i/BindingUtils","SetImageCalls: $setImageCalls")
-        Glide.with(imgView.context)
+        val imgUri = it.toUri().buildUpon().scheme("https").build()
+        Log.i("i/BindingUtils","URI: $imgUri")
+        Glide.with(this.context)
             .load(imgUri)
-            .into(imgView)
+//            .apply(RequestOptions().placeholder("image placeholder"))
+            .into(this)
     }
 }
+//private var setImageCalls = 0
+//
+//@BindingAdapter("imageUrl")
+//fun setImage(imgView: ImageView,imgUrl: String?) {
+//    imgUrl?.let {
+//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+//
+//        setImageCalls++
+//        Log.i("i/BindingUtils","SetImageCalls: $setImageCalls")
+//        Glide.with(imgView.context)
+//            .load(imgUri)
+//            .into(imgView)
+//    }
+//}
