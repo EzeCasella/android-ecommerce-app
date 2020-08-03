d5e5982730a293409ebe272dba68a0cdd1d6bb83
Ezequiel Casella <ezequiel.casella@decemberlabs.com>
2020-08-03 17:33:09 -0300

    Passing right trough the token screen if there is already a token storaged

--- app/src/main/java/com/example/ecommerce/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/ecommerce/presentation/login/LoginFragment.kt
@@ -27,26 +27,27 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        get_token_button.setOnClickListener {
-            login_progressBar.visibility = View.VISIBLE
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        var token = sharedPref?.getString(getString(R.string.saved_token_key), null)
 
-            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
-            var token = sharedPref?.getString(getString(R.string.saved_token_key), null)
+        if (token == null) {
+            get_token_button.setOnClickListener {
+                login_progressBar.visibility = View.VISIBLE
 
-            if ( token == null) {
                 TokenGenerator.generateAuthToken {
                     Log.i("i/LoginFragment", "Token NULO")
                     token = "Bearer $it"
                     sharedPref?.edit()?.putString(getString(R.string.saved_token_key), token)?.commit()
                     login_progressBar.visibility = View.INVISIBLE
-                    this.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMarketListFragment())
+                    this.findNavController()
+                        .navigate(LoginFragmentDirections.actionLoginFragmentToMarketListFragment())
                 }
-            } else {
-                Log.i("i/LoginFragment", "Token NO es NULO, es: $token")
-                login_progressBar.visibility = View.INVISIBLE
-                this.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMarketListFragment())
             }
+        } else {
+            Log.i("i/LoginFragment", "Token NO es NULO, es: $token")
+            this.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMarketListFragment())
         }
+
 
 //        TODO: LiveData loading en ViewModel, para observar y cambiar visibility de progress_bar
     }
