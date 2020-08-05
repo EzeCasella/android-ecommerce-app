14ffd9a76aaea11d27c2f5551233c42a9705551b
Ezequiel Casella <ezequiel.casella@decemberlabs.com>
2020-08-05 10:52:37 -0300

    Cleaning logs and unused imports

--- app/src/main/java/com/example/ecommerce/marketlist/MarketListFragment.kt
+++ app/src/main/java/com/example/ecommerce/marketlist/MarketListFragment.kt
@@ -8,27 +8,19 @@ import androidx.fragment.app.Fragment
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SnapHelper
 import com.example.ecommerce.R
 import com.example.ecommerce.databinding.MarketListFragmentBinding
-import kotlinx.android.synthetic.main.list_item_category.view.*
-import kotlinx.android.synthetic.main.list_item_product.*
-import kotlinx.android.synthetic.main.market_list_fragment.*
 
 class MarketListFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MarketListFragment()
         var searchBarText: String? = ""
     }
 
@@ -89,11 +81,9 @@ class MarketListFragment : Fragment() {
         })
 
         marketListViewModel.cartProducts.observe(viewLifecycleOwner, Observer {
-            Log.i("i/MarketListFragment","Hubo algun cambio en prodsAmount: $it")
             productAdapter.submitProductsList(marketListViewModel.cartLines.value, binding.searchBar.query.toString())
         })
         productAdapter.submitProductsList(marketListViewModel.cartLines.value, searchBarText)
-        Log.i("i/MarketListFragment", "Searchabr query: ${searchBarText}")
 
         binding.searchBar.apply {
             imeOptions = EditorInfo.IME_ACTION_DONE
@@ -104,8 +94,6 @@ class MarketListFragment : Fragment() {
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-                    Log.i("i/MarketListFragment", "Query text changed")
-                    Log.i("i/MarketListFragment", "searchbar text: ${newText}")
                     searchBarText = newText
                     if (newText.isNullOrEmpty()) {
                         binding.bannerList.visibility = View.VISIBLE
@@ -122,9 +110,6 @@ class MarketListFragment : Fragment() {
                     val imm =
                         activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
                     imm?.hideSoftInputFromWindow(view.windowToken, 0)
-                    Log.i("i/MarketListFragment", "No tiene FOCO")
-                } else {
-                    Log.i("i/MarketListFragment", "Tiene FOCO")
                 }
             }
         }
